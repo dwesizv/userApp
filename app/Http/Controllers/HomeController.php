1db3de16288f91209a7786c6b54ed4b06f988fad
dwesizv <dwes@ieszaidinvergeles.org>
2022-11-29 20:58:17 +0000

    version tres

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,21 +8,11 @@ use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+
+    public function __construct() {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index() {
         return view('home', ['user' => Auth::user()]);
     }
