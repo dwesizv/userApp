4e378895376b26b787995019a8999c5dafbaa4d4
dwesizv <dwes@ieszaidinvergeles.org>
2022-11-29 20:55:56 +0000

    version tres

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,12 +23,47 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
+    public function index() {
         return view('home', ['user' => Auth::user()]);
     }
     
     function update(Request $request) {
-        /// por implementar
+        $validatedData = $this->validateInput($request);
+        $message = 'User data has been updated.';
+        $sendEmail = false;
+        $user = Auth::user();
+        $user->name = $request->name;
+        if($request->password != null && Hash::check($request->old_password, $user->password)) {
+            $user->password = Hash::make($request->input('password'));
+        } elseif($request->password != null) {
+            return back()->withInput()->withErrors([
+                                                'message' => 'User hasn\'t been updated',
+                                                'old_password' => 'Old password does not match password.']);
+        }
+        if($request->email != $user->email) {
+            $user->email = $request->email;
+            $user->email_verified_at = null;
+            $sendEmail = true;
+        }
+        try {
+            $user->update();
+            if($sendEmail) {
+                $user->sendEmailVerificationNotification();
+            } 
+        } catch(\Exception $e) {
+            return back()
+                     ->withInput()
+                     ->withErrors(['message' => 'An unexpected error occurred while updating.']);
+        }
+        return redirect('home')->with('message', $message);
+    }
+
+    private function validateInput(Request $request) {
+        return $request->validate([
+            'email'         => 'required|email',
+            'name'          => 'required|min:2|max:100',
+            'old_password'  => 'nullable|min:8',
+            'password'      => 'nullable|min:8|confirmed',
+        ]);
     }
 }
